59ab6377a4618b898824a2607bdf87296de976c3
Chetan Anand <chetan108@outlook.com>
2024-09-15 15:51:12 +0530

    initial static portfolio website

---
+++ app/fonts/fonts.ts
@@ -0,0 +1,12 @@
+import { Pixelify_Sans, Inria_Sans } from "next/font/google";
+
+export const pixelify_Sans = Pixelify_Sans({
+    subsets: ["latin"],
+    display: "swap",
+});
+
+export const inria_Sans = Inria_Sans({
+    subsets: ["latin"],
+    display: "swap",
+    weight: ["400"],
+});
--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,126 @@
 import Image from "next/image";
+import { pixelify_Sans } from "./fonts/fonts";
+import github from "@/app/assets/Github.png";
+import bookmarks_io from "@/app/assets/bookmarks-io.png";
+import super_app from "@/app/assets/supper-app.png";
+import pocket_notes from "@/app/assets/pocket-notes.png";
+import rock_paper_scissors from "@/app/assets/rock-paper-scissors.png";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    return (
+        <main className="max-w-3xl p-1 m-auto">
+            <div className="hero flex mt-8">
+                <div className="left flex flex-col justify-center">
+                    <h1 className={`${pixelify_Sans.className} text-7xl`}>
+                        Chetan Anand
+                    </h1>
+                    <p className="w-3/4 mt-2">
+                        My name is Chetan Anand, here I showcase all my
+                        projects, interests, currently learning tech stacks.
+                    </p>
+                    <div className="links flex items-center gap-3 mt-4">
+                        <button className="bg-theme-grey text-black px-4 py-2 rounded-md font-bold">
+                            Contact me
+                        </button>
+                        <div className="github bg-theme-grey aspect-square p-2 rounded-full">
+                            <Image
+                                src={github}
+                                alt="Github image"
+                                width={"25"}
+                            />
+                        </div>
+                    </div>
+                </div>
+                <div className="right">
+                    <div className="w-[200px] h-[200px] rounded-full flex justify-center items-center bg-custom-grey">
+                        <p
+                            className={`${pixelify_Sans.className} text-8xl text-black`}
+                        >
+                            CA
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="projects mt-3">
+                <div
+                    className={`${pixelify_Sans.className} border-b-2 border-slate-50 text-xl`}
+                >
+                    projects
+                </div>
+                <div className="projects-container p-2 grid grid-cols-2 gap-3">
+                    <div className="bg-theme-grey text-black p-3 rounded-md ">
+                        <h1
+                            className={`${pixelify_Sans.className} text-2xl border-b-2 border-black`}
+                        >
+                            bookmarks-io
+                        </h1>
+                        <div className="rounded-sm mt-2">
+                            <Image
+                                className="rounded-sm"
+                                src={bookmarks_io}
+                                alt="bookmarks-io screenshot"
+                            />
+                        </div>
+                        <p className="font-bold text-sm mt-2">
+                            Web app to save bookmarks online, arrange in
+                            different groups.
+                        </p>
+                    </div>
+                    <div className="bg-theme-grey text-black p-3 rounded-md ">
+                        <h1
+                            className={`${pixelify_Sans.className} text-2xl border-b-2 border-black`}
+                        >
+                            supper-app
+                        </h1>
+                        <div className="rounded-sm mt-2">
+                            <Image
+                                className="rounded-sm"
+                                src={super_app}
+                                alt="bookmarks-io screenshot"
+                            />
+                        </div>
+                        <p className="font-bold text-sm mt-2">
+                            Fontend app design that contains a lot of small apps
+                            like weather, timer, news etc.
+                        </p>
+                    </div>
+                    <div className="bg-theme-grey text-black p-3 rounded-md ">
+                        <h1
+                            className={`${pixelify_Sans.className} text-2xl border-b-2 border-black`}
+                        >
+                            pocket-notes
+                        </h1>
+                        <div className="rounded-sm mt-2">
+                            <Image
+                                className="rounded-sm"
+                                src={pocket_notes}
+                                alt="bookmarks-io screenshot"
+                            />
+                        </div>
+                        <p className="font-bold text-sm mt-2">
+                            Notes taking web app using react.
+                        </p>
+                    </div>
+                    <div className="bg-theme-grey text-black p-3 rounded-md ">
+                        <h1
+                            className={`${pixelify_Sans.className} text-2xl border-b-2 border-black`}
+                        >
+                            rock-paper-scissors
+                        </h1>
+                        <div className="rounded-sm mt-2">
+                            <Image
+                                className="rounded-sm"
+                                src={rock_paper_scissors}
+                                alt="bookmarks-io screenshot"
+                            />
+                        </div>
+                        <p className="font-bold text-sm mt-2">
+                            Play rock paper scissors game with computer. Made
+                            with vanilla javascript.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
 }
